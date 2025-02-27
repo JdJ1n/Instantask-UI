a85e1c4b3d91227d69903e905fd654b9416b98d9
JudeJin <jjd1024@foxmail.com>
2025-02-27 17:31:08 -0500

    initial commits

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+export default function HomePage() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h1>Hello Next.js!</h1>
+        <p>This is the home page.</p>
+      </div>
+    );
+  }
+  
